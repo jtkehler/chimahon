5a9d0572675cc0c41f121f280fbe90c8231a3cc1
sohilsayed <sohilsayed19@gmail.com>
2026-06-17 04:17:17 +0300

    fix: disable WebView force dark in reader to prevent wrong render for some dark text

--- chimahon/src/main/java/com/canopus/chimareader/ui/reader/ReaderWebView.kt
+++ chimahon/src/main/java/com/canopus/chimareader/ui/reader/ReaderWebView.kt
@@ -123,6 +123,7 @@ fun ReaderWebView(
                 settings.loadWithOverviewMode = false
                 settings.useWideViewPort = true
                 settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+                settings.forceDark = WebSettings.FORCE_DARK_OFF
                 isVerticalScrollBarEnabled = false
                 isHorizontalScrollBarEnabled = false
                 overScrollMode = WebView.OVER_SCROLL_NEVER
